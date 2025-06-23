1dfa07a46a810e2d2e77d6aba9204eafef37f59c
BHAVISHYA-git <52648675+BHAVISHYA-git@users.noreply.github.com>
2025-06-22 22:46:58 -0700

    revert config changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  // basePath: '/trbhavishya',
-  output: "export",  // <=== enables static exports
   experimental: {
     outputFileTracingIncludes: {
       '/articles/*': ['./src/app/articles/**/*.mdx'],
